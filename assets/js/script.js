2f2169370dc0c6305c69f28bcbd644595132c232
Jake Pasternak <jdp.pasternak@gmail.com>
2021-12-18 17:58:41 -1000

    Nav buttons generate HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,18 @@ import { generateContactsPage } from "./generateContactsPage.js";
 import { generateResumePage } from "./generateResumePage.js";
 
 // Initialize sidenav
-$(document).ready(() => $(".sidenav").sidenav());
-
-console.log(generateProjectsPage());
-
-$("#projects").append(generateProjectsPage());
-$("#contact").append(generateContactsPage());
-$("#resume").append(generateResumePage());
+$(document).ready(() => {
+  $(".sidenav").sidenav();
+  $("#projectsNav").on("click", () => {
+    $("main").html(generateProjectsPage());
+  });
+  $("#resumeNav").on("click", () => {
+    $("main").html(generateResumePage());
+  });
+  $("#contactNav").on("click", () => {
+    $("main").html(generateContactsPage());
+  });
+});
 
 $("#resume .activator, #resume .card-reveal .card-title").on("click", (evt) => {
   $(evt.target).closest("div.card").toggleClass("small");
