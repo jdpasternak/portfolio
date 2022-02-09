dd47ac4c8100920e106a08dcf93bedc99dec5e04
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-09 10:47:12 -1000

    contact page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,29 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
 
+const contactInfo = [
+  {
+    icon: `<i class="material-icons">email</i>`,
+    linkText: "Email!",
+    link: "mailto:jdp.pasternak@gmail.com",
+  },
+  {
+    icon: `<i class="material-icons">phone</i>`,
+    linkText: "Call!",
+    link: "tel:1-808-216-3534",
+  },
+  {
+    icon: `<i class="fab fa-linkedin fs-5"></i>`,
+    linkText: "Connect!",
+    link: "https://linkedin.com/in/jakepasternak/",
+  },
+  {
+    icon: `<i class="fab fa-github fs-5"></i>`,
+    linkText: "Follow!",
+    link: "https://github.com/jdpasternak/",
+  },
+];
+
 // Hompeage
 router.get("/", (req, res) => {
   res.render("homepage");
@@ -31,7 +54,7 @@ router.get("/resume", (req, res) => {
 });
 
 router.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", { contacts: contactInfo });
 });
 
 module.exports = router;
