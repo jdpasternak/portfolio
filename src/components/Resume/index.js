93dcecf358c3714a2126bf170cfe1d110805c979
Jake Pasternak <jdp.pasternak@gmail.com>
2022-04-17 18:19:25 -0600

    adds downloadable PDF to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,12 +4,17 @@ import CertificationList from "../CertificationList";
 import WorkList from "../WorkList";
 import EducationList from "../EducationList";
 
+import { Link } from "react-router-dom";
+
 const Resume = (resumeData) => {
   const { summary, skills, certifications, work, education } = resumeData;
 
   return (
     <div className="row">
       <div className="col s12 m9 l10">
+        <Link to="/assets/Jake Pasternak.pdf" target="_blank" download>
+          Download
+        </Link>
         <Summary data={summary} />
         <SkillList data={skills} />
         <CertificationList data={certifications} />
