35475b2d242b295031f7834abb4832db7ba94eb1
Jake Pasternak <jdp.pasternak@gmail.com>
2022-04-17 19:08:37 -0600

    adds scrollspy to Resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,6 +4,7 @@ import CertificationList from "../CertificationList";
 import WorkList from "../WorkList";
 import EducationList from "../EducationList";
 
+
 import { Link } from "react-router-dom";
 
 const Resume = (resumeData) => {
@@ -12,14 +13,40 @@ const Resume = (resumeData) => {
   return (
     <div className="row">
       <div className="col s12 m9 l10">
-        <Link to="/assets/Jake Pasternak.pdf" target="_blank" download>
-          Download
-        </Link>
         <Summary data={summary} />
         <SkillList data={skills} />
         <CertificationList data={certifications} />
         <WorkList data={work} />
         <EducationList data={education} />
+      </div>
+      <div className="col hide-on-small-only m3 l2 fixed">
+        <ul className="section table-of-contents">
+          <li>
+            <a href="#summary">Summary</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#certifications">Certifications</a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <Link
+              to="/assets/Jake Pasternak.pdf"
+              className="btn white-text"
+              target="_blank"
+              download
+            >
+              Download
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
