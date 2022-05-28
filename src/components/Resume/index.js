c7267fc4869110e0f058256af979c0b8c97319ea
Jake Pasternak <jdp.pasternak@gmail.com>
2022-05-28 17:55:49 -0400

    updates summary, skills, certifications sections

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,52 +1,138 @@
-import Summary from "../Summary";
-import SkillList from "../SkillList";
-import CertificationList from "../CertificationList";
-import WorkList from "../WorkList";
-import EducationList from "../EducationList";
+import {
+  Divider,
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Chip,
+  ListItemIcon,
+  Card,
+  CardContent,
+  Grid,
+  CardMedia,
+  CardActions,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  AccordionActions,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ExpandMore } from "@mui/icons-material";
 
-
-import { Link } from "react-router-dom";
-
-const Resume = (resumeData) => {
-  const { summary, skills, certifications, work, education } = resumeData;
-
+const Resume = ({ summary, skills, certifications, education, work }) => {
   return (
     <div className="row">
       <div className="col s12 m9 l10">
-        <Summary data={summary} />
-        <SkillList data={skills} />
-        <CertificationList data={certifications} />
-        <WorkList data={work} />
-        <EducationList data={education} />
-      </div>
-      <div className="col hide-on-small-only m3 l2 fixed">
-        <ul className="section table-of-contents">
-          <li>
-            <a href="#summary">Summary</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#certifications">Certifications</a>
-          </li>
-          <li>
-            <a href="#work">Work</a>
-          </li>
-          <li>
-            <a href="#education">Education</a>
-          </li>
-          <li>
-            <Link
-              to="/assets/Jake Pasternak.pdf"
-              className="btn white-text"
-              target="_blank"
-              download
-            >
-              Download
-            </Link>
-          </li>
-        </ul>
+        <Container>
+          <Typography variant="h4" component="h2">
+            Summary
+          </Typography>
+          <Typography variant="p" component="p">
+            {summary}
+          </Typography>
+          <Typography variant="h4" component="h2" mt={3}>
+            Skills and Technologies
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            alignItems="stretch"
+            justifyContent="center"
+            spacing={2}
+          >
+            {skills.map((skill) => (
+              <>
+                <Grid item xs={12} md={4} key={skill.group}>
+                  <Card>
+                    <CardContent>
+                      <Divider key={skill.group} textAlign="left">
+                        <Chip label={skill.group} />
+                      </Divider>
+                      <List>
+                        {skill.skillsList.map((item) => (
+                          <ListItem key={item.name}>
+                            <ListItemIcon>
+                              <FontAwesomeIcon icon={item.icon} />
+                            </ListItemIcon>
+                            <ListItemText primary={item.name} />
+                          </ListItem>
+                        ))}
+                      </List>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </>
+            ))}
+          </Grid>
+          <Typography variant="h4" component="h2" mt={3}>
+            Certifications
+          </Typography>
+          <Grid
+            container
+            spacing={2}
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="stretch"
+          >
+            {certifications.map((certification) => (
+              <Grid item key={certification.name} xs={6} sm={4} md={3}>
+                <Card sx={{ maxWidth: 200, height: "100%" }}>
+                  <CardMedia
+                    component="img"
+                    image={
+                      certification.logo
+                        ? certification.logo
+                        : "portfolio/assets/images/image-coming-soon.jpg"
+                    }
+                    height={200}
+                    alt={`${certification.name} logo`}
+                  />
+                  <CardContent>
+                    <Typography>{certification.name}</Typography>
+                  </CardContent>
+                  {certification.link && (
+                    <CardActions>
+                      <a
+                        style={{ textDecoration: "none" }}
+                        href={certification.link}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Button size="small">
+                          <Typography>View on Credly</Typography>
+                        </Button>
+                      </a>
+                    </CardActions>
+                  )}
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <Typography variant="h4" component="h2">
+            Work Experience
+          </Typography>
+          <Accordion expanded={true}>
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                Summary
+              </Typography>
+              <Typography>Sub-summary</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>Details</Typography>
+            </AccordionDetails>
+            <AccordionActions>
+              <Button>
+                <Typography>Click here!</Typography>
+              </Button>
+            </AccordionActions>
+          </Accordion>
+          <Typography variant="h4" component="h2">
+            Education
+          </Typography>
+        </Container>
       </div>
     </div>
   );
