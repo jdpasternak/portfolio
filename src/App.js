5e6e5f0e14a5a48e4a19676cc850163a220c7012
Jake Pasternak <jdp.pasternak@gmail.com>
2022-05-29 01:27:43 -0400

    updates background to paper

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-
-import Footer from "./components/Footer";
 import ProjectList from "./components/ProjectList";
 import Resume from "./components/Resume";
 import { resumeData } from "./data/resumeData";
 import Contact from "./components/Contact";
 import About from "./components/About";
-import { Box, Button, Menu, MenuItem, Link } from "@mui/material";
+import { Box, Button, Menu, MenuItem, Link, Paper } from "@mui/material";
 import { useState } from "react";
 import { MenuOutlined } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
@@ -48,68 +46,70 @@ function App() {
           : lightTheme
       }
     >
-      <Router>
-        <header>
-          <Button
-            id="menu-button"
-            aria-controls={open ? "menu" : undefined}
-            arial-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            <MenuOutlined />
-          </Button>
-          <Menu
-            id="menu"
-            aria-labelledby="menu-button"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-          >
-            <Link to="/about" underline="none">
-              <MenuItem>About</MenuItem>
-            </Link>
-            <Link to="/projects" underline="none">
-              <MenuItem>Projects</MenuItem>
-            </Link>
-            <Link to="/resume" underline="none">
-              <MenuItem>Resume</MenuItem>
-            </Link>
-            <Link to="/contact" underline="none">
-              <MenuItem>Contact</MenuItem>
-            </Link>
-          </Menu>
-        </header>
+      <Paper elevation={0}>
+        <Router>
+          <header>
+            <Button
+              id="menu-button"
+              aria-controls={open ? "menu" : undefined}
+              arial-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <MenuOutlined />
+            </Button>
+            <Menu
+              id="menu"
+              aria-labelledby="menu-button"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+            >
+              <Link to="/about" underline="none">
+                <MenuItem>About</MenuItem>
+              </Link>
+              <Link to="/projects" underline="none">
+                <MenuItem>Projects</MenuItem>
+              </Link>
+              <Link to="/resume" underline="none">
+                <MenuItem>Resume</MenuItem>
+              </Link>
+              <Link to="/contact" underline="none">
+                <MenuItem>Contact</MenuItem>
+              </Link>
+            </Menu>
+          </header>
 
-        <main className="container">
-          <div className="row">
-            <Routes>
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/projects" element={<ProjectList />} />
-              <Route
-                exact
-                path="/resume"
-                element={<Resume {...resumeData} />}
-              />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route
-                exact
-                path="*"
-                element={<Navigate to="/about" replace index />}
-              />
-            </Routes>
-          </div>
-        </main>
-        <Box height={20}></Box>
-      </Router>
+          <main className="container">
+            <div className="row">
+              <Routes>
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/projects" element={<ProjectList />} />
+                <Route
+                  exact
+                  path="/resume"
+                  element={<Resume {...resumeData} />}
+                />
+                <Route exact path="/contact" element={<Contact />} />
+                <Route
+                  exact
+                  path="*"
+                  element={<Navigate to="/about" replace index />}
+                />
+              </Routes>
+            </div>
+          </main>
+          <Box height={20}></Box>
+        </Router>
+      </Paper>
     </ThemeProvider>
   );
 }
