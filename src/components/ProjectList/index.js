2767e352002f24e2f352de0dae0d95535497a544
Jake Pasternak <jdp.pasternak@gmail.com>
2022-05-29 19:46:27 -0400

    adds images to project cards, language icons

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -7,23 +7,40 @@ import {
   CardActions,
   Link,
   Button,
+  Container,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { formatName } from "../../helpers";
-import { Img } from "react-image";
+import { Img, useImage } from "react-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCss3,
+  faHtml5,
+  faJava,
+  faJs,
+  faPython,
+} from "@fortawesome/free-brands-svg-icons";
 
-// const Image = (project) => {
-//   const { src } = useImage({
-//     srcList: [
-//       `https://github.com/${project.owner.login}/${project.name}/raw/${project.default_branch}/assets/images/screenshot.PNG`,
-//       `${process.env.PUBLIC_URL}/assets/images/image-coming-soon.jpg`,
-//     ],
-//   });
-//   return <CardMedia src={src} />;
-// };
+const Image = ({ project }) => {
+  const { src } = useImage({
+    srcList: [
+      `https://github.com/${project.owner.login}/${project.name}/raw/${project.default_branch}/assets/images/screenshot.PNG`,
+      `${process.env.PUBLIC_URL}/assets/images/image-coming-soon.jpg`,
+    ],
+  });
+  return <img src={src} alt={project.name} width="100%" />;
+};
 
 const ProjectList = () => {
   const [projects, setProjects] = useState();
+
+  // const { src } = useImage({
+  //   srcList: [
+  //     `https://github.com/${project.owner.login}/${project.name}/raw/${project.default_branch}/assets/images/screenshot.PNG`,
+  //     `${process.env.PUBLIC_URL}/assets/images/image-coming-soon.jpg`,
+  //   ],
+  // });
 
   useEffect(() => {
     fetch(
@@ -33,47 +50,78 @@ const ProjectList = () => {
       .then((data) => setProjects(data));
   }, []);
 
+  const langToIcon = {
+    Java: faJava,
+    HTML: faHtml5,
+    JavaScript: faJs,
+    CSS: faCss3,
+    Python: faPython,
+  };
+
   return (
-    <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}>
-      {projects &&
-        projects.map((project) => (
-          <Card>
-            <Img
-              src={[
-                `https://github.com/${project.owner.login}/${project.name}/raw/${project.default_branch}/assets/images/screenshot.PNG`,
-                `${process.env.PUBLIC_URL}/assets/images/image-coming-soon.jpg`,
-              ]}
-            />
-            <CardContent>
-              <Typography>{formatName(project.name)}</Typography>
-            </CardContent>
-            <CardActions>
-              {project.homepage ? (
-                <Link href={project.homepage} target="_blank" underline="none">
-                  <Button>Live Site</Button>
-                </Link>
-              ) : (
-                <Link
-                  href={`${project.html_url}/archive/refs/heads/${project.default_branch}.zip`}
-                  target="_blank"
-                  rel="noreferrer"
-                  underline="none"
-                >
-                  Download
-                </Link>
-              )}
-              <Link
-                href={project.html_url}
-                target="_blank"
-                rel="noreferrer"
-                underline="none"
-              >
-                GitHub
-              </Link>
-            </CardActions>
-          </Card>
-        ))}
-    </Masonry>
+    <Container>
+      <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}>
+        {projects &&
+          projects
+            .filter((project) => !project.fork)
+            .map((project) => (
+              <Card key={project.name}>
+                {project.homepage ? (
+                  <Link
+                    href={project.homepage}
+                    target="_blank"
+                    underline="none"
+                  >
+                    <Image project={project} />
+                  </Link>
+                ) : (
+                  <Link
+                    href={`${project.html_url}/archive/refs/heads/${project.default_branch}.zip`}
+                    target="_blank"
+                    rel="noreferrer"
+                    underline="none"
+                  >
+                    <Image project={project} />
+                  </Link>
+                )}
+                <CardContent>
+                  <Typography>
+                    <FontAwesomeIcon icon={langToIcon[project.language]} />{" "}
+                    {formatName(project.name)}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  {project.homepage ? (
+                    <Link
+                      href={project.homepage}
+                      target="_blank"
+                      underline="none"
+                    >
+                      <Button>Live Site</Button>
+                    </Link>
+                  ) : (
+                    <Link
+                      href={`${project.html_url}/archive/refs/heads/${project.default_branch}.zip`}
+                      target="_blank"
+                      rel="noreferrer"
+                      underline="none"
+                    >
+                      <Button>Download</Button>
+                    </Link>
+                  )}
+                  <Link
+                    href={project.html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                    underline="none"
+                  >
+                    <Button>GitHub</Button>
+                  </Link>
+                </CardActions>
+              </Card>
+            ))}
+      </Masonry>
+    </Container>
   );
 };
 
